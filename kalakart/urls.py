17472bc6ffc648b582f445a2896c2a2df69173fd
Akash-Rajdhami <akashrd177@gmail.com>
2026-05-17 23:21:48 +0545

    push

--- kalakart/urls.py
+++ kalakart/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from base_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
