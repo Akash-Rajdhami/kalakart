5388be2c9205fab0e5ef4a5228f2a64d534ec8a8
Akash-Rajdhami <akashrd177@gmail.com>
2026-05-19 23:01:49 +0545

    navbar

--- kalakart/settings.py
+++ kalakart/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join (BASE_DIR / "static",)]
+STATICFILES_DIRS = [os.path.join (BASE_DIR / "Static",)]
 
 MEDIA_URL = 'media/'
 
-MEDAI_ROOT = [os.path.join (BASE_DIR / "media",)]
+MEDIA_ROOT = [os.path.join (BASE_DIR / "Media",)]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
