7c28c52ad0226543e52389a2e17c410e0afc7bb6
Akash-Rajdhami <akashrd177@gmail.com>
2026-08-10 22:28:29 +0545

    cart views update

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
+from .models import Cart
 
-# Create your views here.
+
+def AddToCartView(request, id):
+
+    if not request.user.is_authenticated:
+        messages.error(request, "Please login to add products to cart.")
+        return redirect("login")
+
+    product = get_object_or_404(Product, id=id)
+
+    cart_item, created = Cart.objects.get_or_create(
+        user=request.user,
+        product=product
+    )
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    messages.success(
+        request,
+        f"{product.name} added to your cart."
+    )
+
+    return redirect("product-detail", id=product.id)
