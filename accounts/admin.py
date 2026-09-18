13b3b1ba5ab391694f1cecafa7efc7ada2f46aac
Akash-Rajdhami <akashrd177@gmail.com>
2026-09-18 23:13:10 +0545

    accounts addmin updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "user_type",
+        "is_staff",
+        "is_superuser",
+    )
+
+    list_filter = (
+        "user_type",
+        "is_staff",
+        "is_superuser",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
