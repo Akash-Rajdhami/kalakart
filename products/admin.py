013441060843cc0e4f77d6c6a7e730a2c596c6a4
Akash-Rajdhami <akashrd177@gmail.com>
2026-09-19 20:03:19 +0545

    products admin updated

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,25 @@ from django.contrib import admin
 from .models import Product
 
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "category",
+        "price",
+        "stock",
+        "seller",
+        "created_at",
+    )
+
+    list_filter = (
+        "category",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "description",
+        "seller__username",
+    )
